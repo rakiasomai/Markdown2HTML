f9e9b2f3a59a38be160a3803d3ef7795ee8d049d
rakiasomai <1069@holbertonschool.com>
2020-10-06 13:11:27 +0000

    fix

--- markdown2html.py
+++ markdown2html.py
@@ -1,11 +1,11 @@
-!/usr/bin/python3
+#!/usr/bin/python3
 '''Markdown2HTML'''
 import os
 from sys import argv, exit, stderr
 
 
-def run():
-    ''' def run '''
+def get():
+    ''' def get '''
     if len(argv) != 3:
         print("Usage: ./markdown2html.py README.md README.html", file=stderr)
         exit(1)
@@ -28,4 +28,4 @@ def run():
     exit(0)
 
 if __name__ == "__main__":
-    run()
+    get()
